eb4b43a83a68e4f81a0de24596962dca4b79c732
Si Thu Hlaing <fumaingqiang@gmail.com>
2025-07-06 21:36:12 +0200

    project skeleton

---
+++ src/test/java/com/tolimoli/pms/PmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolimoli.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
